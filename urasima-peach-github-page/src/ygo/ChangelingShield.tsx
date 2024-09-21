a75651c5bb95dfb044f045d0d7228ea753e347cd
UrasimaPeach <urasimapeach@gmail.com>
2024-09-21 20:57:15 +0900

    fix 404 and reading message

--- urasima-peach-github-page/src/ygo/ChangelingShield.tsx
+++ urasima-peach-github-page/src/ygo/ChangelingShield.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import markdownit from 'markdown-it';
 
 const ChangelingShield = () => {
-  const [markdownText, setMarkdownText] = useState<string>("# headind")
+  const [markdownText, setMarkdownText] = useState<string>("# 読み込み中")
   useEffect(() => {
     const markdownLoadFunction = async () => {
       const response = await fetch("/markdown/ChangelingShield.md");
